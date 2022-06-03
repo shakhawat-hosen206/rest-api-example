83c8790232da8690bc61cf245a3f9bdf57f7146f
ayeshaparvin <shakhawat.hosen@northsouth.edu>
2022-06-03 14:51:59 +0600

    added nested api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const blockQuote = () =>{
+/* const blockQuote = () =>{
     fetch('https://api.kanye.rest/')
     .then(res => res.json())
     .then(quote => displayQuote(quote))
@@ -11,4 +11,23 @@ const blockQuote = () =>{
 const displayQuote = quote =>{
     const quoteId = document.getElementById('quote');
     quoteId.innerText = quote.quote;
+} */
+
+
+const multipleUser = () =>{
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(data => displayUser(data))
+}
+
+
+const displayUser = user =>{
+    const buddies = user.results;
+    const userId = document.getElementById('user');
+    for(const buddy of buddies){
+         const p = document.createElement('p');
+         p.innerText =` name:${buddy.name.first} ${buddy.name.last} 
+          Email:${buddy.email}`;
+         userId.appendChild(p);
+    }
 }
